55c06e1dcd3db427b9425ac3447f7e6918036b3e
AniBITS <f20250508@pilani.bits-pilani.ac.in>
2026-09-19 13:06:37 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -1,27 +1,47 @@
-// tb.v
-// Starter testbench template -- YOU complete this file.
-
 module tb;
 
-  // TODO: declare the inputs and outputs
+    reg  [2:0] t_sel;
+    wire [7:0] t_dout;
 
-  // TODO: instantiate DUT here
+    // Parameter override
+    lut #(
+        .WIDTH(8),
+        .DEPTH(8)
+    ) U1 (
+        .sel  (t_sel),
+        .dout (t_dout)
+    );
 
-  // Waveform dump configuration (DO NOT CHANGE)
-  string vcd_file;
-  initial begin
-    if ($value$plusargs("vcd=%s", vcd_file)) begin
-      $dumpfile(vcd_file);
-      $dumpvars(0, DUT);
+    // Waveform dump configuration
+    string vcd_file;
+
+    initial begin
+        if ($value$plusargs("vcd=%s", vcd_file)) begin
+            $dumpfile(vcd_file);
+            $dumpvars(0, U1);
+        end
     end
-  end
 
-  initial begin
-    // TODO: apply different input combinations
+    integer i;
 
-  end
+    initial begin
+        for (i = 0; i < 8; i = i + 1) begin
+            t_sel = i;
+            #5;
 
-  initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+            if (t_dout !== i * i)
+                $display("ERROR: sel=%d expected=%d got=%d",
+                         i, i*i, t_dout);
+            else
+                $display("PASS: sel=%d dout=%d",
+                         i, t_dout);
+        end
+
+        $finish;
+    end
+
+    initial begin
+        $monitor($time, " sel=%d | dout=%d", t_sel, t_dout);
+    end
 
 endmodule
